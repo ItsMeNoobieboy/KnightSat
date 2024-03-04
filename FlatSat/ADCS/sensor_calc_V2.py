7d9f59a70421e47e4e93a25b91b3f2a6ca1b4160
FuigisBald <116591416+FuigisBald@users.noreply.github.com>
2024-03-03 20:10:45 -0500

    all my fellas

--- FlatSat/ADCS/sensor_calc_V2.py
+++ FlatSat/ADCS/sensor_calc_V2.py
@@ -11,8 +11,6 @@ relies on your sensor functions here.
 #import libraries
 import time
 import math
-import numpy as np
-import os
 import board
 import busio
 from adafruit_lsm6ds.lsm6dsox import LSM6DSOX as LSM6DS
@@ -23,61 +21,63 @@ i2c = busio.I2C(board.SCL, board.SDA)
 accel_gyro = LSM6DS(i2c)
 mag = LIS3MDL(i2c)
 
+def convert_to_degrees(radians):
+    return radians * 180 / math.pi
 
 #Activity 1: RPY based on accelerometer and magnetometer
 def roll_am(accelX,accelY,accelZ):
-    roll = math.atan(accelY/((accelX**2+accelZ**2)**.5))
-    #TODO
+    roll = math.atan(accelY / (accelX**2+accelZ**2) ** .5)
     return roll
 
 def pitch_am(accelX,accelY,accelZ):
-    pitch = math.atan(accelX/((accelY**2+accelZ**2)**.5))
-    #TODO
+    pitch = math.atan(accelX / (accelY**2+accelZ**2) ** .5)
     return pitch
 
 def yaw_am(accelX,accelY,accelZ,magX,magY,magZ):
-    #TODO 
-    mag_x = magX * math.cos(pitch_am(accelX,accelY,accelZ)) 
-    mag_x += magY * math.sin(roll_am(accelX,accelY,accelZ)) * math.sin(pitch_am(accelX,accelY,accelZ))
-    mag_x += magZ * math.cos(roll_am(accelX,accelY,accelZ)) * math.sin(pitch_am(accelX,accelY,accelZ))
-    mag_y = magY * math.cos(roll_am(accelX, accelY, accelZ)) - magZ*math.sin(roll_am(accelX, accelY, accelZ))
-    return math.atan(-1 * mag_y / mag_x)
+    mag_x = magX*math.cos(pitch_am(accelX,accelY,accelZ))
+    mag_x += magY*math.sin(roll_am(accelX,accelY,accelZ))*math.sin(pitch_am(accelX,accelY,accelZ))
+    mag_x += magZ*math.cos(roll_am(accelX,accelY,accelZ))*math.sin(pitch_am(accelX,accelY,accelZ))
+    mag_y = magY*math.cos(roll_am(accelX, accelY, accelZ)) - magZ*math.sin(roll_am(accelX, accelY, accelZ))
+    yaw = -mag_y/mag_x
+    return math.atan(accelZ / (accelY**2+accelX**2) ** .5)
 
 #Activity 2: RPY based on gyroscope
 # only going to utilize magnetometer & accelerometer measurements, doesn't need this functionality
-def roll_gy(prev_angle, delT, gyro): return
-def pitch_gy(prev_angle, delT, gyro): return
-def yaw_gy(prev_angle, delT, gyro): return
+def roll_gy(prev_angle, delT, gyro):
+    #TODO NOT DONE
+    return
+def pitch_gy(prev_angle, delT, gyro):
+    #TODO NOT DONE
+    return
+def yaw_gy(prev_angle, delT, gyro):
+    #TODO NOT DONE
+    return
 
 #Activity 3: Sensor calibration
 def calibrate_mag():
     #TODO: Set up lists, time, etc
     magX, magY, magZ = mag.magnetic #gauss
+    
     startT = time.time()
+
     print("Preparing to calibrate magnetometer. Please wave around.")
-    maxX = magX
-    minX = magX
-    maxY = magY
-    minY = magY
-    maxZ = magZ
-    minZ = magZ
+    maxX = minX = magX
+    maxY = minY = magY
+    maxZ = minZ = magZ
+
     while(time.time() - startT < 3):
-        if magX > maxX:
-            maxX = magX
-        elif magX < minX:
-            minX = magX
-        if magY > maxY:
-            magY = magY
-        elif magY < minY:
-            minY = magY
-        if magZ > maxZ:
-            maxZ = magZ
-        elif magZ < minZ:
-            minZ = magZ 
+        maxX = max(maxX, magX)
+        minX = min(minX, magX)
+        maxY = max(maxY, magY)
+        minY = min(minY, magY)
+        maxZ = max(maxZ, magZ)
+        minZ = min(minZ, magZ)
+
     print("Calibrating...")
     offsetX = (maxX+minY)/2
     offsetY = (maxY+minY)/2
     offsetZ = (maxZ+minZ)/2
+    
     #TODO: Calculate calibration constants
     print("Calibration complete.")
     return [offsetX,offsetY,offsetZ]
@@ -102,15 +102,20 @@ def set_initial(mag_offset = [0,0,0]):
     print("Preparing to set initial angle. Please hold the IMU still.")
     time.sleep(3)
     print("Setting angle...")
+
     accelX, accelY, accelZ = accel_gyro.acceleration #m/s^2
     magX, magY, magZ = mag.magnetic #gauss
+
     #Calibrate magnetometer readings. Defaults to zero until you
     #write the code
     magX = magX - mag_offset[0]
     magY = magY - mag_offset[1]
     magZ = magZ - mag_offset[2]
+
     roll = roll_am(accelX, accelY,accelZ)
     pitch = pitch_am(accelX,accelY,accelZ)
     yaw = yaw_am(accelX,accelY,accelZ,magX,magY,magZ)
+
     print("Initial angle set.")
+
     return [roll,pitch,yaw]
