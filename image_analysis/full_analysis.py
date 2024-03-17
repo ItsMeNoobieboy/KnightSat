42debae3e2fb71ec366092a05fdb24d57f4b1a41
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2024-03-16 21:39:49 -0400

    turned off led

--- image_analysis/full_analysis.py
+++ image_analysis/full_analysis.py
@@ -15,6 +15,8 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 LED_PIN = 26
 GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.output(LED_PIN, False)
+
 
 # VARIABLES
 THRESHOLD = 15  # Any desired value from the accelerometer
